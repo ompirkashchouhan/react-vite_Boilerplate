c80bf0f4e9352b65bee337641324feea2023acd7
ompirkashchouhan <omsuther6@gmail.com>
2025-09-01 12:52:39 +0500

    added_props

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "../components/Button";
+
 
 function About() {
+  const clickMe = () => alert("Hi, there")
   return (
     <center>
       <h1>Welcome to About Page </h1>
+      <Button title={"Say, Hi"} clickMe={clickMe}/>
     </center>
   );
 }
--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Button from "../components/Button";
 
 function Contact() {
+  const clickMe = () => alert("Greetings")
   return (
     <center>
       <h1>Welcome to Contact Page </h1>
+      <Button title={"Wellcome"} clickMe={clickMe} />
     </center>
   );
 }
